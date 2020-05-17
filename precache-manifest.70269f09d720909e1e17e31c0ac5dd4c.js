26b74c0392fe69fbdd924807a91ac7c8bc1a93fd
alex3007 <48695087+alex3007@users.noreply.github.com>
2020-05-17 18:15:19 +0300

    changed

---
+++ precache-manifest.70269f09d720909e1e17e31c0ac5dd4c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e39588c0b0b41db8802bc83569483cea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d626fb937e46ed4ca8a1",
+    "url": "/static/css/2.84862d57.chunk.css"
+  },
+  {
+    "revision": "afd93416e346efa046c8",
+    "url": "/static/css/main.3d2fd7ab.chunk.css"
+  },
+  {
+    "revision": "d626fb937e46ed4ca8a1",
+    "url": "/static/js/2.c5e3905f.chunk.js"
+  },
+  {
+    "revision": "afd93416e346efa046c8",
+    "url": "/static/js/main.ff14feab.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b48569238eeb0d62de28a5d056b16bef",
+    "url": "/static/media/Gabriela-Regular.b4856923.ttf"
+  },
+  {
+    "revision": "2f127e880e576febcb5177dc22ad68a3",
+    "url": "/static/media/kolo.2f127e88.png"
+  }
+]);
